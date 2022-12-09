66afbae78faa302b1ca760c2629880140c8fceba
Lenovo <vasilevski_aleksei@mail.ru>
2022-12-09 22:20:30 +0300

    creating a new product(+wrapper)

---
+++ src/app/create-product/create-product.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ProductsService } from '../shared/services/products.service';
+@Component({
+  selector: 'app-createproduct',
+  templateUrl: './create-product.component.html',
+  styleUrls: ['./create-product.component.scss'],
+})
+export class CreateProductComponent implements OnInit {
+  formGroup: FormGroup;
+  constructor(
+    private router: Router,
+    private readonly fb: FormBuilder,
+    private readonly productsService: ProductsService,
+  ) {
+    this.formGroup = this.fb.group({
+      name: ['', Validators.required],
+      manufacturer: ['', Validators.required],
+      width: [''],
+      height: [''],
+      depth: [''],
+      country_of_origin: [''],
+    });
+  }
+
+  createproduct() {
+    this.productsService.createproduct(this.formGroup.value).subscribe({
+      next: () => this.router.navigateByUrl('products'),
+      error: (err) => console.error(err),
+    });
+  }
+  ngOnInit(): void {}
+}
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../shared/interfaces/product.interface';
+import { ProductsService } from '../shared/services/products.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss'],
+})
+export class ProductsComponent implements OnInit {
+  products: IProduct[] = [];
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'manufacturer',
+    'weight',
+    'height',
+    'depth',
+    'image',
+    'country_of_origin',
+  ];
+  constructor(
+    private readonly productsService: ProductsService,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+  getProducts() {
+    this.productsService.getProducts().subscribe({
+      next: (products) => (this.products = products),
+      error: (err) => console.error(err),
+    });
+  }
+  gocreate() {
+    this.router.navigateByUrl('createproducts');
+  }
+}
