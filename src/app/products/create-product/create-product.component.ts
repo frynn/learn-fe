0c54ae1e04bcccc9fac301ca1cc58c610d5c167c
jeka0 <latosh02@mail.ru>
2022-12-16 18:25:28 +0300

    Implemented adding an image

--- src/app/products/create-product/create-product.component.ts
+++ src/app/products/create-product/create-product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ProductsService } from '../../shared/services/products.service';
+import { ImagesService } from '../../shared/services/images.service';
 @Component({
   selector: 'app-create-product',
   templateUrl: './create-product.component.html',
@@ -24,17 +25,34 @@ export class CreateProductComponent {
       Validators.pattern(/^[0-9,.]+$/),
     ),
     country_of_origin: this.fb.control<string>(''),
+    image: this.fb.control<string>(''),
   });
 
   constructor(
     private router: Router,
     private readonly fb: FormBuilder,
     private readonly productsService: ProductsService,
+    private readonly imagesService: ImagesService,
   ) {}
   createProduct() {
     this.productsService.createProduct(this.formGroup.value).subscribe({
       next: () => this.router.navigateByUrl('products'),
       error: (err) => console.error(err),
     });
+  }
+
+  onLoadImage(event: Event) {
+    const formdata = new FormData();
+    const target = <HTMLInputElement>event.target;
+    if (target.files) {
+      formdata.append('file', target.files[0]);
+      this.imagesService.newImage(formdata).subscribe({
+        next: (result) =>
+          this.formGroup.patchValue({
+            image: result.filename,
+          }),
+        error: (err) => console.error(err),
+      });
+    }
   }
 }
