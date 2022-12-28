a0ea48c2f03280e9c06ff80415ac3362e4f1a14b
frynn <grodnenec04@mail.ru>
2022-12-28 20:17:59 +0300

    *added greeting *added buttons for routing

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IUser } from '../shared/interfaces';
+import { AuthService } from '../shared/services/auth.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
+  constructor(private readonly authService: AuthService) {}
 
-  ngOnInit(): void {}
+  profile: IUser | null = null;
+  message: string = '';
+  hours = new Date().getHours();
+
+  messageInput() {
+    if (this.hours >= 5 && this.hours < 12) {
+      this.message = 'Доброе утро';
+    } else if (this.hours >= 12 && this.hours < 18) {
+      this.message = 'Добрый день';
+    } else if (this.hours >= 18 && this.hours < 24) {
+      this.message = 'Добрый вечер';
+    } else if (this.hours >= 24 && this.hours < 5) {
+      this.message = 'Доброй ночи';
+    }
+  }
+
+  getProfile() {
+    this.authService.getProfile().subscribe({
+      next: (profile) => (this.profile = profile),
+      error: (err) => console.error(err),
+    });
+  }
+
+  ngOnInit(): void {
+    this.messageInput();
+    this.getProfile();
+  }
 }
