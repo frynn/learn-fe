57ad2254b8b6cf6b1ec83e766826168b29fbda07
scriptSQD <scriipt.fun@gmail.com>
2022-12-29 23:14:24 +0300

    * Provide new tests - Provide tests for AuthInterceptor and UsersService

--- src/app/shared/services/users.service.spec.ts
+++ src/app/shared/services/users.service.spec.ts
@@ -1,16 +1,170 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UsersService } from './users.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { mockArray, mockUser, randomEmail, randomNumber } from '@unit-tests';
+import { environment } from '../../../environments/environment';
+import { IPaginatedResponse } from '../interfaces/paginated-response';
+import { IUser } from '@interfaces';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(UsersService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('should get users', () => {
+    it('by id', function () {
+      const id = randomNumber();
+      const user = mockUser();
+
+      service
+        .getUser(id.toString())
+        .subscribe((response) => expect(response).toEqual(user));
+
+      const req = httpController.expectOne(`${environment.apiUrl}/users/${id}`);
+      req.flush(user);
+
+      const method = req.request.method;
+      expect(method).toBe('GET');
+    });
+
+    it('without pagination', function () {
+      const userAmount = 45;
+      const users = mockArray(userAmount, () => mockUser());
+      const reqBody: IPaginatedResponse<IUser[]> = {
+        total: userAmount,
+        data: users,
+      };
+
+      service.getUsers().subscribe({
+        next: (response) => {
+          expect(response.total).toEqual(userAmount);
+          expect(response.start).not.toBeDefined();
+          expect(response.limit).not.toBeDefined();
+          expect(response.data).toEqual(users);
+        },
+      });
+
+      const req = httpController.expectOne(`${environment.apiUrl}/users`);
+      req.flush(reqBody);
+
+      const params = req.request.params;
+      expect(params.keys()).toEqual([]);
+
+      const method = req.request.method;
+      expect(method).toBe('GET');
+    });
+
+    it('with pagination', function () {
+      const userAmount = 45;
+      const users = mockArray(userAmount, () => mockUser());
+
+      // pagination
+      const page = 2;
+      const limit = 10;
+      const start = page * limit;
+
+      // mock response
+      const slicedUsers = users.slice(start, start + limit);
+      const reqBody: IPaginatedResponse<IUser[]> = {
+        total: userAmount,
+        start: start,
+        limit: limit,
+        data: slicedUsers,
+      };
+
+      service.getUsers(page, limit).subscribe({
+        next: (response) => {
+          expect(response.total).toBe(userAmount);
+          expect(response.start).toBe(start);
+          expect(response.limit).toBe(limit);
+          expect(response.data).toEqual(slicedUsers);
+        },
+      });
+
+      const req = httpController.expectOne(
+        `${environment.apiUrl}/users?start=${start}&limit=${limit}`,
+      );
+      req.flush(reqBody);
+
+      const params = req.request.params;
+      expect(params.get('start')).toBe(start.toString());
+      expect(params.get('limit')).toBe(limit.toString());
+
+      const method = req.request.method;
+      expect(method).toBe('GET');
+    });
+  });
+
+  it('should post new user', function () {
+    const user = mockUser();
+    const id = randomNumber().toString();
+
+    service
+      .newUser(user)
+      .subscribe((response) =>
+        expect(response).toEqual({ ...user, userId: id }),
+      );
+
+    const req = httpController.expectOne(`${environment.apiUrl}/users`);
+    req.flush({ ...user, userId: id });
+
+    const body = req.request.body;
+    expect(body).toEqual(user);
+
+    const method = req.request.method;
+    expect(method).toBe('POST');
+  });
+
+  it('should update user', function () {
+    const user = mockUser();
+    const newEmail = randomEmail();
+    const update: Partial<IUser> = { email: newEmail };
+
+    service.updateUser(user.userId, update).subscribe({
+      next: (response) => {
+        expect(response.email).toBe(newEmail);
+        expect(response).toEqual({ ...user, email: newEmail });
+      },
+    });
+
+    const req = httpController.expectOne(
+      `${environment.apiUrl}/users/${user.userId}`,
+    );
+    req.flush({ ...user, email: newEmail });
+
+    const body = req.request.body;
+    expect(body).toEqual(update);
+
+    const method = req.request.method;
+    expect(method).toBe('PATCH');
+  });
+
+  it('should delete user', function () {
+    const id = randomNumber().toString();
+
+    service
+      .deleteUser(id)
+      .subscribe((response) => expect(response).toEqual({ result: 'ok' }));
+
+    const req = httpController.expectOne(`${environment.apiUrl}/users/${id}`);
+    req.flush({ result: 'ok' });
+
+    const method = req.request.method;
+    expect(method).toBe('DELETE');
   });
 });
--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { IUser } from '../interfaces';
+import { IUser } from '@interfaces';
 import { IPaginatedResponse } from '../interfaces/paginated-response';
 
 @Injectable({
@@ -33,13 +33,14 @@ export class UsersService {
     return this.http.post<IUser>(`${environment.apiUrl}/users`, payload);
   }
 
-  updateUser(id: string, payload: IUser): Observable<IUser> {
+  updateUser(id: string, payload: Partial<IUser>): Observable<IUser> {
     return this.http.patch<IUser>(`${environment.apiUrl}/users/${id}`, payload);
   }
 
   getUser(id: string): Observable<IUser> {
     return this.http.get<IUser>(`${environment.apiUrl}/users/${id}`);
   }
+
   deleteUser(id: string): Observable<{ result: string }> {
     return this.http.delete<{ result: string }>(
       `${environment.apiUrl}/users/${id}`,
