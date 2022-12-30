813eef649e066a7e49cbdbc72061210a1d4b6bc7
AndreyRogachevich <andrey.rogachevich@azati.com>
2022-12-30 11:59:55 +0300

    AuthService tests

--- src/app/shared/services/auth.service.spec.ts
+++ src/app/shared/services/auth.service.spec.ts
@@ -1,16 +1,78 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { HttpClient } from '@angular/common/http';
+import { cold } from 'jasmine-marbles';
+import { mockLoginPayload, mockUser, randomString } from '@unit-tests';
+import { environment } from '../../../environments/environment';
 
 describe('AuthService', () => {
   let service: AuthService;
+  const mockHttpClient = {
+    get: jasmine.createSpy(),
+    post: jasmine.createSpy(),
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [],
+      providers: [
+        AuthService,
+        { provide: HttpClient, useValue: mockHttpClient },
+      ],
+    });
     service = TestBed.inject(AuthService);
+
+    mockHttpClient.get.calls.reset();
+    mockHttpClient.post.calls.reset();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeDefined();
+  });
+
+  it('`getProfile` should call http.get', () => {
+    const user = mockUser();
+    mockHttpClient.get.and.returnValue(cold('(c|)', { c: user }));
+    const expected$ = cold('(c|)', { c: user });
+    expect(service.getProfile()).toBeObservable(expected$);
+    expect(mockHttpClient.get).toHaveBeenCalledWith(
+      `${environment.apiUrl}/profile`,
+    );
+  });
+
+  it('`login` should call http.post and store token in service and to localStorage', () => {
+    service.access_token = '';
+    const token = randomString();
+    const spy = spyOn(localStorage, 'setItem');
+    mockHttpClient.post.and.returnValue(
+      cold('(c|)', { c: { access_token: token } }),
+    );
+    const expected$ = cold('(c|)', { c: { access_token: token } });
+    const payload = mockLoginPayload();
+    expect(service.login(payload)).toBeObservable(expected$);
+    expect(mockHttpClient.post).toHaveBeenCalledWith(
+      `${environment.apiUrl}/auth/login`,
+      payload,
+    );
+    expect(service.access_token).toEqual(token);
+    expect(spy).toHaveBeenCalledWith('access_token', token);
+  });
+
+  it('`isAuth` should return true if assess_token exists', () => {
+    service.access_token = '';
+    expect(service.isAuth()).toBeFalsy();
+    service.access_token = randomString();
+    expect(service.isAuth()).toBeTruthy();
+  });
+
+  it('`logout` should clear access_token', () => {
+    service.profile = mockUser();
+    service.access_token = randomString();
+    const spy = spyOn(localStorage, 'removeItem');
+    service.logout();
+    expect(service.access_token).toEqual('');
+    expect(service.profile).toBeNull();
+    expect(spy).toHaveBeenCalled();
   });
 });
--- src/unit-tests/user.mock.ts
+++ src/unit-tests/user.mock.ts
@@ -1,5 +1,6 @@
 import { IUser } from '@interfaces';
 import { randomEmail, randomString } from '@unit-tests';
+import { ILoginPayload } from '../app/shared/services/auth.service';
 
 export const mockUser = (override: Partial<IUser> = {}): IUser => ({
   userId: randomString(),
@@ -9,5 +10,13 @@ export const mockUser = (override: Partial<IUser> = {}): IUser => ({
   phone: randomString(),
   site: randomString(),
   avatar: randomString(),
+  ...override,
+});
+
+export const mockLoginPayload = (
+  override: Partial<ILoginPayload> = {},
+): ILoginPayload => ({
+  username: randomString(),
+  password: randomString(),
   ...override,
 });
