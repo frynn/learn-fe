4be6d0250cd4f5f71e5a61cc255c2108b1ee41b2
jeka0 <latosh02@mail.ru>
2022-12-30 14:41:14 +0300

    added tests on login component

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,22 +1,72 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
 import { LoginComponent } from './login.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { of } from 'rxjs';
+import { randomBoolean, randomString } from '@unit-tests';
+import { cold, getTestScheduler } from 'jasmine-marbles';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
+  let router: Router;
+  const mockAuthService = {
+    login: jasmine.createSpy().and.returnValue(of()),
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
+      imports: [RouterTestingModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+      providers: [
+        FormBuilder,
+        { provide: AuthService, useValue: mockAuthService },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
+    router = TestBed.inject(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.showPassword).toBeFalse();
+    expect(component.formGroup.value).toEqual({ username: '', password: '' });
+  });
+
+  it('`togglePasswordVisibility` should toggle showPassword', () => {
+    const testValue = randomBoolean();
+    component.showPassword = testValue;
+    component.togglePasswordVisibility();
+    expect(component.showPassword).toBe(!testValue);
+  });
+
+  describe('`login`', () => {
+    it('should navigate to another page', () => {
+      const spy = spyOn(router, 'navigateByUrl');
+      mockAuthService.login.and.returnValue(cold('(a|)', { a: {} }));
+      getTestScheduler().run(() => {
+        component.login();
+      });
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it('should console.error in case of error', () => {
+      const spy = spyOn(console, 'error');
+      const spyNavigate = spyOn(router, 'navigateByUrl');
+      mockAuthService.login.and.returnValue(
+        cold('#', {}, new Error('Mistake')),
+      );
+      getTestScheduler().run(() => {
+        component.login();
+      });
+      expect(spy).toHaveBeenCalled();
+      expect(spyNavigate).not.toHaveBeenCalled();
+    });
   });
 });
